a82b0c39c1ace6d943097f4b534d1149e717b883
QUERY-GANI <ahmadxgani01@gmail.com>
2021-06-11 18:54:05 +0700

    change native to hapi framework, ref: https://www.dicoding.com/academies/261/tutorials/14722

--- server.js
+++ server.js
@@ -1,58 +1,17 @@
-const http = require('http');
-const PORT = 5000;
-const HOST = "localhost";     
-
-const requestListener = (request, response) => {
-    response.setHeader("Content-Type", "application/json");
-    response.setHeader("X-Powered-By", "NodeJS");
-    const { method, url } = request;
-    if(url === "/") {
-        // TODO 2: logika respons bila url bernilai '/'
-        if (method === "GET") {
-            response.statusCode = 200;
-            response.end(JSON.stringify({
-                message: "Ini adalah homepage"
-            }))
-        } else {
-            response.statusCode = 404;
-            response.end(JSON.stringify({
-                message: `Halaman tidak dapat diakses dengan ${method} request`
-            }))
-        }
-    } else if(url === "/about") {
-        // TODO 3: logika respons bila url bernilai '/about'
-        if (method === "GET") {
-            response.statusCode = 200;
-            response.end(JSON.stringify({
-                message: "Halo! Ini adalah halaman about"
-            }))
-        } else if (method === "POST") {
-            response.statusCode = 200;
-            const body = [];
-            request.on("data", (chunk) => body.push(chunk))
-            request.on("end", () => {
-                const { name } = JSON.parse(Buffer.concat(body).toString());
-                response.end(JSON.stringify({
-                    message: `Halo, ${name}! Ini adalah halaman about`
-                }))
-            })
-        } else {
-            response.statusCode = 404;
-            response.end(JSON.stringify({
-                message: `Halaman tidak dapat diakses dengan ${method} request`
-            }))
-        }
-    } else {
-        // TODO 1: logika respons bila url bukan '/' atau '/about'
-        response.statusCode = 404;
-        response.end(JSON.stringify({
-            message: "Halaman tidak ditemukan!"
-        }))
-    }
+const Hapi = require('@hapi/hapi');
+const routes = require('./routes');
+ 
+ 
+const init = async () => {
+    const server = Hapi.server({
+        port: 5000,
+        host: 'localhost',
+    });
+ 
+    server.route(routes);
+ 
+    await server.start();
+    console.log(`Server berjalan pada ${server.info.uri}`);
 };
  
-const server = http.createServer(requestListener);
-
-server.listen(PORT, HOST, () => {
-    console.log(`Server running on ${HOST}:${PORT}`);
-})
+init();
