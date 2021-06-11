ba615476589296bc8b516878e62226f450904245
QUERY-GANI <ahmadxgani01@gmail.com>
2021-06-11 14:32:24 +0700

    using if statement for method & endpoint, Ref https://www.dicoding.com/academies/261/tutorials/14692

--- server.js
+++ server.js
@@ -1,30 +1,40 @@
 const http = require('http');
-const PORT = 3000;
+const PORT = 5000;
 const HOST = "localhost";     
-/**
- * Logika untuk menangani dan menanggapi request dituliskan pada fungsi ini
- * 
- * @param request: objek yang berisikan informasi terkait permintaan
- * @param response: objek yang digunakan untuk menanggapi permintaan
- */
+
 const requestListener = (request, response) => {
     response.setHeader('Content-Type', 'text/html');
-    response.statusCode = 200;
     const { method, url } = request;
-
-    if(method === 'GET') {
-        response.end('<h1>Hello!</h1>');
-    }
-    if(method === 'POST') {
-        let body = [];
-        request.on("data", (chunk) => {
-            body.push(chunk)
-        })
-        request.on("end", () => {
-            body = Buffer.concat(body).toString()
-            const { name } = JSON.parse(body);
-            response.end(`<h1>Hai, ${name}!</h1>`);
-        })
+    if(url === '/') {
+        // TODO 2: logika respons bila url bernilai '/'
+        if (method === "GET") {
+            response.statusCode = 200;
+            response.end("<h1>Ini adalah homepage</h1>")
+        } else {
+            response.statusCode = 404;
+            response.end(`<h1>Halaman tidak dapat diakses dengan ${method} request</h1>`)
+        }
+    } else if(url === "/about") {
+        // TODO 3: logika respons bila url bernilai '/about'
+        if (method === "GET") {
+            response.statusCode = 200;
+            response.end("<h1>Halo! Ini adalah halaman about</h1>")
+        } else if (method === "POST") {
+            response.statusCode = 200;
+            const body = [];
+            request.on("data", (chunk) => body.push(chunk))
+            request.on("end", () => {
+                const { name } = JSON.parse(Buffer.concat(body).toString());
+                response.end(`<h1>Halo, ${name}! Ini adalah halaman about</h1>`)
+            })
+        } else {
+            response.statusCode = 404;
+            response.end(`<h1>Halaman tidak dapat diakses dengan ${method} request</h1>`)
+        }
+    } else {
+        // TODO 1: logika respons bila url bukan '/' atau '/about'
+        response.statusCode = 404;
+        response.end("<h1>Halaman tidak ditemukan!</h1>")
     }
 };
  
