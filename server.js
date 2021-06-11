2931338cac7dbaead58c9117d26a4821dfe51a11
QUERY-GANI <ahmadxgani01@gmail.com>
2021-06-11 18:20:06 +0700

    change html to json, ref: https://www.dicoding.com/academies/261/tutorials/14702

--- server.js
+++ server.js
@@ -3,39 +3,51 @@ const PORT = 5000;
 const HOST = "localhost";     
 
 const requestListener = (request, response) => {
-    response.setHeader('Content-Type', 'text/html');
-    response.setHeader('X-Powered-By', 'NodeJS');
+    response.setHeader("Content-Type", "application/json");
+    response.setHeader("X-Powered-By", "NodeJS");
     const { method, url } = request;
-    if(url === '/') {
+    if(url === "/") {
         // TODO 2: logika respons bila url bernilai '/'
         if (method === "GET") {
             response.statusCode = 200;
-            response.end("<h1>Ini adalah homepage</h1>")
+            response.end(JSON.stringify({
+                message: "Ini adalah homepage"
+            }))
         } else {
             response.statusCode = 404;
-            response.end(`<h1>Halaman tidak dapat diakses dengan ${method} request</h1>`)
+            response.end(JSON.stringify({
+                message: `Halaman tidak dapat diakses dengan ${method} request`
+            }))
         }
     } else if(url === "/about") {
         // TODO 3: logika respons bila url bernilai '/about'
         if (method === "GET") {
             response.statusCode = 200;
-            response.end("<h1>Halo! Ini adalah halaman about</h1>")
+            response.end(JSON.stringify({
+                message: "Halo! Ini adalah halaman about"
+            }))
         } else if (method === "POST") {
             response.statusCode = 200;
             const body = [];
             request.on("data", (chunk) => body.push(chunk))
             request.on("end", () => {
                 const { name } = JSON.parse(Buffer.concat(body).toString());
-                response.end(`<h1>Halo, ${name}! Ini adalah halaman about</h1>`)
+                response.end(JSON.stringify({
+                    message: `Halo, ${name}! Ini adalah halaman about`
+                }))
             })
         } else {
             response.statusCode = 404;
-            response.end(`<h1>Halaman tidak dapat diakses dengan ${method} request</h1>`)
+            response.end(JSON.stringify({
+                message: `Halaman tidak dapat diakses dengan ${method} request`
+            }))
         }
     } else {
         // TODO 1: logika respons bila url bukan '/' atau '/about'
         response.statusCode = 404;
-        response.end("<h1>Halaman tidak ditemukan!</h1>")
+        response.end(JSON.stringify({
+            message: "Halaman tidak ditemukan!"
+        }))
     }
 };
  
